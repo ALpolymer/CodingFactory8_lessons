db61f42d3feb4dc452984192a5d4079e0445ac34
Alexandros Polymeros <alpolymer@gmail.com>
2025-05-22 18:29:03 +0300

    feat: added algorithms

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -1,4 +1,33 @@
 package gr.aueb.cf.ch12;
 
 public class Main {
+    /*
+    --- Η αρχική κλάση Student ---
+    public class Student {
+    int id;
+    String firstname;
+    String lastname;
+}
+     */
+    public static void main(String[] args) {
+        /*
+        alice -> Student instance()
+        Student() -> (argumentless) constructor method (Αρχικοποιεί τα πέδία της κλάσης)
+        Από τη στιγμή που αρχικοποιείται το instance μέσω του constructor
+        χωρις παραμέτρους τα πεδία παιρνουν default τιμές:
+        id =0;
+        firstname = null;
+        lastname = null;
+         */
+        Student alice = new Student();
+
+        alice.id =10;              //set id
+        alice.firstname = "Alice"; //set firstname
+        alice.lastname = "W.";     //set lastname
+
+        System.out.println(alice.id);           //get id
+        System.out.println(alice.firstname);    //get firstname
+        System.out.println(alice.lastname);     //get firstname
+
+    }
 }
