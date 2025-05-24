f61f9b7556b0bf12aa9e45c4ebef6d5b12150dd5
Alexandros Polymeros <alpolymer@gmail.com>
2025-05-24 19:09:34 +0300

    add Utility Class

---
+++ src/gr/aueb/cf/ch14/HelloUtil.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch14;
+
+/**
+ * Utility classes
+ * Private constructor
+ * Stateless
+ * static methods
+ */
+public class HelloUtil {
+    private HelloUtil(){
+
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello Coding!");
+    }
+}
