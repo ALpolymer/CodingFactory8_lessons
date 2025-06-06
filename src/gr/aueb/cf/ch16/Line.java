c0edb408dadc773f6192357bbaf46083d33f8fad
Alexandros Polymeros <alpolymer@gmail.com>
2025-06-06 14:31:37 +0300

    feat: add Line class

---
+++ src/gr/aueb/cf/ch16/Line.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch16;
+
+public class Line {
+    private double length;
+
+    Line(){
+
+    }
+
+    Line(double length){
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+}
